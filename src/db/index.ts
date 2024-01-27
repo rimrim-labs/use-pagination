348f067470ff640615630da4460720b64f64d0af
rimrim990 <lim990718@naver.com>
2024-01-27 20:48:36 +0900

    config: mongoose 디버깅 설정

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,10 @@ import mongoose from 'mongoose'
 
 export async function connect() {
   try {
+    mongoose.set('debug', (collectionName, method, query, doc) => {
+      console.log(`${collectionName}.${method}`, JSON.stringify(query), doc)
+    })
+
     await mongoose.connect('mongodb://127.0.0.1:27017/test')
   } catch (err) {
     console.error('failed to create connection')
